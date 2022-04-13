00b5fb2af51b94d03e9df6fd04a032ad5f0d12c2
Himanshu  Raj <himanshuraj9194@gmail.com>
2022-04-13 23:26:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function test(){
+    // document.getElementById("rightDiv").textContent;
+    // console.log(document.getElementById("rightDiv").textContent);
+    // This 
+    const  response = httpGet("https://io.adafruit.com/api/v2/Himanshu00/feeds/spp01/data/last?x-aio-key=aio_VRcK98jj5clu3aBYOvCfwM3TcHEX")
+    console.log(response)
+    const obj = JSON.parse(response)
+    console.log(obj.value)
+
+    const str = 
+
+    valueArray = ["Himanshu Raj you are doing gread work how are you doign today", "Priyanshu Raj", "Anuj Kumar","ramses","This is the last item of the list "];
+    document.getElementById("rightDiv").innerHTML = `<div id="div1">${obj.value} </div>
+                                                     <div id="div2"> ${valueArray[1]} </div>
+                                                     <div id="div3"> ${valueArray[2]} </div>
+                                                     <div id="div4"> ${valueArray[3]} </div>
+                                                     <div id="div5"> ${valueArray[4]} </div>`
+  
+}
+
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
