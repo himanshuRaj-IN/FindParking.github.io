75c58bca11fe83a79c1fe12f1ce666e9950bbf1b
Himanshu Raj <himanshuraj9194@gmail.com>
2022-04-14 06:47:37 +0530

    adding more api calls

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ function test(){
     // document.getElementById("rightDiv").textContent;
     // console.log(document.getElementById("rightDiv").textContent);
     // This 
-    const  response = httpGet("https://io.adafruit.com/api/v2/Himanshu00/feeds/spp01/data/retain")
-    console.log(response)
+    const  response1 = httpGet("https://io.adafruit.com/api/v2/Himanshu00/feeds/spp01/data/retain")
+    const  response2 = httpGet("https://io.adafruit.com/api/v2/Himanshu00/feeds/spp02/data/retain")
+    const  response3 = httpGet("https://io.adafruit.com/api/v2/Himanshu00/feeds/spp03/data/retain")
+    const  response4 = httpGet("https://io.adafruit.com/api/v2/Himanshu00/feeds/spp04/data/retain")
+    const  response5 = httpGet("https://io.adafruit.com/api/v2/Himanshu00/feeds/spp05/data/retain")
+    console.log(response1)
+    console.log(response2)
+    console.log(response3)
+    console.log(response4)
+    console.log(response5)
 
     const str = 
 
     valueArray = ["Himanshu Raj you are doing gread work how are you doign today", "Priyanshu Raj", "Anuj Kumar","ramses","This is the last item of the list "];
-    document.getElementById("rightDiv").innerHTML = `<div id="div1">${response} </div>
-                                                     <div id="div2"> ${valueArray[1]} </div>
-                                                     <div id="div3"> ${valueArray[2]} </div>
-                                                     <div id="div4"> ${valueArray[3]} </div>
-                                                     <div id="div5"> ${valueArray[4]} </div>`
+    document.getElementById("rightDiv").innerHTML = `<div id="div1"> ${response1} </div>
+                                                     <div id="div2"> ${response2} </div>
+                                                     <div id="div3"> ${response3} </div>
+                                                     <div id="div4"> ${response4} </div>
+                                                     <div id="div5"> ${response5} </div>`
   
 }
 
